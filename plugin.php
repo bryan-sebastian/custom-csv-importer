ce4b2b5c927f48324cd2f138b604b09d48ce2c74
Bryan Sebastian <bryanrsebastian@gmail.com>
2018-08-15 10:48:10 +0800

    Update the plugin details

--- plugin.php
+++ plugin.php
@@ -1,10 +1,12 @@
 <?php
 /*
 Plugin Name: Custom CSV Importer
-Description: Imports data from CSV Files.
+Plugin URI:	 https://github.com/bryan-sebastian/custom-csv-importer
+Description: A wordpress plugin that can import a list of vouchers with order. Note that this plugin is for developers only because the saving of data are customized depends on the structure of data that will save in the database.
 Version:     1.0.0
 Author:      Bryan Sebastian
 Author URI:  https://bryan-sebastian.github.io/
+License:	 MIT license - http://www.opensource.org/licenses/mit-license.php
 */
 
 if ( !defined( 'WPINC' ) )
